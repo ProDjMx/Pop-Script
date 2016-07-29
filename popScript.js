026230e0ae2c951e9c944a52cd581a89055b9b68
ProDjMx <nico3478@hotmail.fr>
2016-07-29 16:50:09 -0400

    Update popScript.js

--- popScript.js
+++ popScript.js
@@ -1,14 +1,19 @@
 setTimeout(function(){
-    API.chatLog(" Personal script loaded correctly ")
+    API.chatLog(" Pop Script Loaded ")
 }, 2000);
 
 // Found on internet
 var fullUrl = document.location.href;
 var emplacement = document.location.href.substring(fullUrl.lastIndexOf( "/" )+1 );
 // ---
+var lvl = API.getUser().level;
 
 API.on(API.CHAT_COMMAND, function(cmd){
     if(cmd.toLowerCase() == "/loadplugspam"){
+      if (lvl < 20){ /* For test purpose */
+        API.chatLog("Sorry, it requires to be level 4 or above");
+      } else {
         $.getScript('https://cdn.rawgit.com/ProDjMx/Pop-Script/master/plugSpam.js');
+      }
     }
 })
