44fc279ede600a989dbc29c51c221bc5858a364e
ProDjMx <nico3478@hotmail.fr>
2016-07-29 15:47:06 -0400

    Create popScript.js

---
+++ popScript.js
@@ -0,0 +1,14 @@
+setTimeout(function(){
+    API.chatLog(" Personal script loaded correctly ")
+}, 2000);
+
+// Found on internet
+var fullUrl = document.location.href;
+var emplacement = document.location.href.substring(fullUrl.lastIndexOf( "/" )+1 );
+// ---
+
+API.on(API.CHAT_COMMAND, function(cmd){
+    if(cmd.toLowerCase() == "/loadplugspam"){
+        $.getScript('');
+    }
+})
