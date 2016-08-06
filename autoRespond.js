bb503f4f58ba29314fa15e85c1aee01792da7a8b
ProDjMx <nico3478@hotmail.fr>
2016-08-06 02:58:44 -0400

    Create autoRespond.js

---
+++ autoRespond.js
@@ -0,0 +1,9 @@
+// autoRespond by ProDjMx
+// TODO: Start the actual script
+
+// Found on internet
+var fullUrl = document.location.href;
+var emplacement = document.location.href.substring(fullUrl.lastIndexOf( "/" )+1 );
+// ---
+
+// NEED TO BE WORKED ON
