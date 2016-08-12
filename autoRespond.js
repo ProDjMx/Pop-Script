e4e218cbc18f731947e64647498cb60febf94108
ProDjMx <nico3478@hotmail.fr>
2016-08-12 00:05:20 -0400

    Update autoRespond.js

--- autoRespond.js
+++ autoRespond.js
@@ -1,9 +1,1 @@
-// autoRespond by ProDjMx
-// TODO: Start the actual script
-
-// Found on internet
-var fullUrl = document.location.href;
-var emplacement = document.location.href.substring(fullUrl.lastIndexOf( "/" )+1 );
-// ---
-
-// NEED TO BE WORKED ON
+// FIND OUT IT WAS PERSONAL, might be shared in the future.
